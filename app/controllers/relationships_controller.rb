d4d31c475cd52c0eacd63e3ad2dbf83bccf44a38
mikanfirst <mmkkmkk4869@gmail.com>
2021-05-11 14:44:58 +0000

    [add]ルーティング、コントローラ、モデル

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,7 @@
+class RelationshipsController < ApplicationController
+  def followers
+  end
+
+  def followings
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  devise_for :admins
   devise_for :users
+
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationships, only:[:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+  resources :posts do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
