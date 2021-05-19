7fff62fce8fd3e778c6aa7f3b4f2445ea2e6bb91
mikanfirst <mmkkmkk4869@gmail.com>
2021-05-19 09:31:52 +0000

    [fix]詳細ページエラー修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,7 +5,7 @@ class PostCommentsController < ApplicationController
   def create
     article = Article.find(params[:article_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.article_image = comment.article_image_id
+    comment.article_id = article.id
     comment.save
     redirect_to article_path(article)
   end
@@ -14,7 +14,7 @@ class PostCommentsController < ApplicationController
     PostComment.find_by(id: params[:id], article_id: params[:article_id]).destroy
     redirect_to article_path(params[:article_id])
   end
-  
+
   private
   def post_comment_params
     params.require(:post_comment).permit(:comment)
