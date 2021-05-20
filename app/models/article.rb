57018446f5b8f30cdc5ff313bb8e10dd63f2af7d
mikanfirst <mmkkmkk4869@gmail.com>
2021-05-20 07:12:53 +0000

    [add]フォロー機能

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
@@ -7,9 +8,9 @@ class Article < ApplicationRecord
   validates :body, presence: true
 
   attachment :article_image
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,26 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
 
   attachment :profile_image
-
-
+  
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followed, through: :reverse_of_relationships, source: :follower
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
 
 end
