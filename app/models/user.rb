3b94617951acbf14f7699dd18f8b950f26f178f9
mikanfirst <mmkkmkk4869@gmail.com>
2021-05-21 13:35:25 +0000

    [fix]フォローワーページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,23 +10,23 @@ class User < ApplicationRecord
 
   attachment :profile_image
   
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
-  # 自分がフォローされる（被フォロー）側の関係性
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 被フォロー関係を通じて参照→自分をフォローしている人
-  has_many :follow, through: :reverse_of_relationships, source: :follower
-  
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-  
+
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
